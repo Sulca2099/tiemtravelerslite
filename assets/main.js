2ba0679cc927457350149fef13598d4b4f14434b
Sulca2099 <Lucasaswenson@gmail.com>
2026-04-23 11:55:27 -0700

    Add gowith and justtalk functions
    Added gowith and justtalk functions for room navigation and conversation popups.

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,3 @@
 function justgo(){}//function to go to another room
+function gowith(){}//function for going to room, but it requires something to execute
+function justtalk(){}//function to make a popup for the converstions
