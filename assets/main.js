713a7a2cca53347e14d802809f2e8efc314dba48
Sulca2099 <Lucasaswenson@gmail.com>
2026-04-23 12:08:18 -0700

    Change export to non-export for functions in main.js

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,5 @@
-export function justgo(urlfor){//function to go to another room
+function justgo(urlfor){//function to go to another room
   window.location.href=urlfor;//change the url to the room
 }
-export function gowith(){}//function for going to room, but it requires something to execute
-export function justtalk(){}//function to make a popup for the converstions
+function gowith(){}//function for going to room, but it requires something to execute
+function justtalk(){}//function to make a popup for the converstions
