622136ab48eccda8e0181e864150655cd2adc957
Sulca2099 <Lucasaswenson@gmail.com>
2026-04-23 12:04:03 -0700

    Export functions justgo, gowith, and justtalk

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,5 @@
-function justgo(urlfor){//function to go to another room
+export function justgo(urlfor){//function to go to another room
   window.location.href=urlfor;//change the url to the room
 }
-function gowith(){}//function for going to room, but it requires something to execute
-function justtalk(){}//function to make a popup for the converstions
+export function gowith(){}//function for going to room, but it requires something to execute
+export function justtalk(){}//function to make a popup for the converstions
