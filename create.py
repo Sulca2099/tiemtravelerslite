71e771abc1af8f1c82bdc2f9092b4a7578d18100
Sulca2099 <Lucasaswenson@gmail.com>
2026-04-23 11:34:29 -0700

    Refactor create.py for improved HTML output
    Updated HTML template generation and button action handling.

--- create.py
+++ create.py
@@ -9,7 +9,7 @@ while a!="exit":
 		funct=input("button function: ")
 		textofbutton=input("button text: ")
 		actions+=f"\t\t<button class=\"{["claimitem","goroom","executeact"][int(input("claim item=0\rgoto room=1\rdo something=2\r"))]}\"onclick=\"{funct}\">{textofbutton}</button><!--this is a button that preforms the function of {textofbutton} it calls the function {funct}-->\r"
-template=f"<!DOCTYPE html>\r<html lang="en">\r\t<head><!--defines the head of the html file-->\r\t\t<title>{titletext}</title><!--set the title-->\r\t\t<link rel=\"icon\" type=\"image/x-icon\" href=\"assets/favicon.ico\"><!--set the favicon-->\r\t\t<link rel=\"stylesheet\" href=\"assets/main.css\"><!--set the css file destinaton-->\r\t\t<script type=\"module\" src=\"assets/main.js\"><!--import the javascript-->\r\t</head>\r\t<body><!--define the body of the Website-->\r\t\t<p>\r\t\t\t{description}\r\t\t</p> \r{actions}\r\t</body>\r</html>"
+template=f"<!DOCTYPE html>\r<html lang="en"><!--the head for the html file set to english-->\r\t<head><!--defines the head of the html file-->\r\t\t<title>{titletext}</title><!--set the title-->\r\t\t<link rel=\"icon\" type=\"image/x-icon\" href=\"assets/favicon.ico\"><!--set the favicon-->\r\t\t<link rel=\"stylesheet\" href=\"assets/main.css\"><!--set the css file destinaton-->\r\t\t<script type=\"module\" src=\"assets/main.js\"><!--import the javascript-->\r\t</head>\r\t<body><!--define the body of the Website-->\r\t\t<p>\r\t\t\t{description}\r\t\t</p> \r{actions}\r\t</body>\r</html>"
 fileofyou=open(input("filename: "),"w")
 with fileofyou as files:
 	files.write(template)
