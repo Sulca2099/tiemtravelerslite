2ef98e7269bb26b4007430e9268064694cc96cb8
Sulca2099 <Lucasaswenson@gmail.com>
2026-04-23 12:01:55 -0700

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,5 @@
-function justgo(){}//function to go to another room
+function justgo(urlfor){//function to go to another room
+  window.location.href=urlfor;//change the url to the room
+}
 function gowith(){}//function for going to room, but it requires something to execute
 function justtalk(){}//function to make a popup for the converstions
