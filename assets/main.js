0c70aae965027b20ef1dc627289af91f85bcbbb7
Sulca2099 <Lucasaswenson@gmail.com>
2026-04-24 11:30:55 -0700

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,14 @@
+
 function justgo(urlfor){//function to go to another room
   window.location.href=urlfor;//change the url to the room
 }
 function gowith(){}//function for going to room, but it requires something to execute
 function justtalk(){}//function to make a popup for the converstions
+function claim(item,amount,location){
+  if(sessionStorage.getItem(location)==null){
+    sessionStorage.setItem(item,sessionStorage.getItem(item)+amount);
+    sessionStorage.getItem(location)="true";
+  } else{
+    alert("you took this item already, find something else...");
+  }
+}
